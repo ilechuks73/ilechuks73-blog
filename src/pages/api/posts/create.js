38ab59a445bb26fdaf7c78c07c4e5c3a52b85cc5
ilechukwu joshua <ilechuks73@gmail.com>
2024-05-31 18:03:21 +0100

    added error handling

--- src/pages/api/posts/create.js
+++ src/pages/api/posts/create.js
@@ -8,27 +8,35 @@ export const config = {
 };
 
 export default async function handler(request, response) {
-  await utils.connectDb();
-  let permalink = `https://blog.ilechuks73.com/${request.body.title.replaceAll(
-    " ",
-    "-"
-  )}`;
+  try {
+    await utils.connectDb();
+    let permalink = `https://blog.ilechuks73.com/${request.body.title.replaceAll(
+      " ",
+      "-"
+    )}`;
 
-  let post = await models.post.findOne({ permalink: permalink });
-  if (post) {
+    let post = await models.post.findOne({ permalink: permalink });
+    if (post) {
+      return response.status(400).json({
+        message: "Post with this title already exists",
+        statusCode: "DUPLICATEPOSTTITLE",
+        data: null,
+      });
+    }
+    request.body.permalink = permalink;
+    await models.post.create({ ...request.body });
+    return response.status(200).json({
+      message: "success",
+      data: {
+        posts: posts,
+      },
+      statusCode: "OK",
+    });
+  } catch (error) {
     return response.status(400).json({
-      message: "Post with this title already exists",
-      statusCode: "DUPLICATEPOSTTITLE",
+      message: "Unable to process this request",
+      statusCode: "ERROR",
       data: null,
     });
   }
-  request.body.permalink = permalink;
-  await models.post.create({ ...request.body });
-  return response.status(200).json({
-    message: "success",
-    data: {
-      posts: posts,
-    },
-    statusCode: "OK",
-  });
 }
