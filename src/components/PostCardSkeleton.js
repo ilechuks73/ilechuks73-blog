63ca8f0c65d72b6057161ee2415aaacb46b3600f
ilechukwu joshua <ilechuks73@gmail.com>
2024-06-02 19:18:34 +0100

    added skeleton loader

--- src/components/PostCardSkeleton.js
+++ src/components/PostCardSkeleton.js
@@ -3,7 +3,7 @@ import { Skeleton as MuiSkeleton } from "@mui/material";
 export default function PostCardSkeleton(props) {
   return (
     <div>
-      <MuiSkeleton height={150} className={""} />
+      <MuiSkeleton height={200} className={""} />
       <MuiSkeleton className={"h-[20px]"} />
       <MuiSkeleton className={"h-[17px] w-[70%]"} />
       <MuiSkeleton className={"h-[35px] w-[30%]"} />
