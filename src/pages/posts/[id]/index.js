3a87fa935974ba916d410f6bdf88ffbd56888d36
ilechukwu joshua <ilechuks73@gmail.com>
2024-06-02 03:44:14 +0100

    clean up working dir

--- src/pages/posts/[id]/index.js
+++ src/pages/posts/[id]/index.js
@@ -3,6 +3,11 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import services from "@/services";
 import { enqueueSnackbar } from "notistack";
+import { MdOutlineWavingHand } from "react-icons/md";
+import { MdOutlineMarkEmailRead } from "react-icons/md";
+import { GoArrowUpRight } from "react-icons/go";
+import { Button as MuiButton, TextField as MuiTextField } from "@mui/material";
+import moment from "moment";
 
 function Post(props) {
   const router = useRouter();
@@ -23,7 +28,56 @@ function Post(props) {
   //   return () => {};
   // }, [router.query.id]);
 
-  return <h1>{post?.title}</h1>;
+  return (
+    <div>
+      <Head>
+        <title>{post.title}</title>
+        <meta name="description" content={post.title} />
+      </Head>
+      <div className={""}>
+        <div className={"font-roboto max-w-[1000px] mx-4 md:mx-10 lg:mx-auto"}>
+          <div className={"flex justify-between items-center py-3"}>
+            <img
+              alt={"ilechuks73"}
+              className={"h-[30px] md:h-[50px] cursor-pointer"}
+              src={
+                "https://res.cloudinary.com/geergregrgege/image/upload/v1716754334/assets/images/xsp4qwpdyc0j0s2805vf.png"
+              }
+            />
+            <div className={"flex"}>
+              <MuiButton
+                endIcon={<GoArrowUpRight className={"font-light"} />}
+                className={
+                  "!text-black !font-light !font-poppins !rounded-none"
+                }
+              >
+                {"Portfolio"}
+              </MuiButton>
+              <MuiButton
+                disabled={true}
+                className={
+                  "!text-black !rounded-none !border-l !border-l-black !border-solid !font-poppins"
+                }
+              >
+                {"Blog"}
+              </MuiButton>
+            </div>
+          </div>
+          <div className={"flex gap-x-6 w-full"}>
+            <div className={"w-[100%] md:w-[60%] pb-10"}>
+              <h1 className={"text-[30px] font-bold pt-20"}>{post.title}</h1>
+              <p className="text-sm text-gray-500">
+                {moment(post.createdAt).format("LL")}
+              </p>
+            </div>
+          </div>
+          <div className={"w-[100%] md:w-[40%] hidden md:block"}>
+            <div dangerouslySetInnerHTML={{ __html: post.body }} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export async function getServerSideProps(context) {
